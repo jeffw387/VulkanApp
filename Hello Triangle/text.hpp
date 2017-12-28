3e5006cd386782c356c50283712787ba51f52fd7
Jeff Wright <jeffw387@gmail.com>
2017-12-27 21:46:48 -0800

    text engine progress

--- Hello Triangle/text.hpp
+++ Hello Triangle/text.hpp
@@ -9,10 +9,16 @@
 using FontID = size_t;
 using FontSize = size_t;
 
+struct FontData
+{
+    FT_Face face;
+    std::map<FontSize, std::map<char, 
+};
+
 template <typename... fontTypes>
 class TextEngine
 {
-    std::map<FontID, std::map<FontSize
+    std::map<FontID, std::map<FontSize, std::map<char, vke::Image2D>>> fontMaps;
 
 public:
 
@@ -22,7 +28,7 @@ public:
         return static_cast<FontID>(MPL::IndexOf<FontType, fontTypes...>::value);
     }
 
-    void Init()
+    void Init(std::vector<std::string> fontPaths, vke::LogicalDevice& device)
     {
         auto error = FT_Init_FreeType(&library);
         if (error != 0)
@@ -30,34 +36,23 @@ public:
             std::cerr << "Error initializing Freetype!";
         }
 
-        FT_UInt glyphID;
-        glyphIndices.push_back(FT_Get_First_Char(face, &glyphID));
+        FontID currentFont = 0;
 
-        while (glyphID)
+        for (auto& fontPair : fontMaps)
         {
-            glyphIndices.push_back(FT_Get_Next_Char(face, glyphIndices.back(), &glyphID));
-        }
-    }
+            auto fontPath = fontPaths[currentFont];
+            for (auto& sizePair : fontPair.second)
+            {
+                FT_UInt glyphID;
+                glyphIndices.push_back(FT_Get_First_Char(face, &glyphID));
 
-    template <size_t ID, size_t size>
-    void loadFace(const char* fontPath, FT_Long faceIndex)
-    {
-        FT_Face face;
+                while (glyphID)
+                {
+                    glyphIndices.push_back(FT_Get_Next_Char(face, glyphIndices.back(), &glyphID));
+                }
+            }
+        }
         
-        auto error = FT_New_Face(library, fontPath, faceIndex, &face);
-        if (error == FT_Err_Unknown_File_Format)
-        {
-            std::cerr << "Error loading font AeroviasBrasil, font format not supported.\r\n";
-        }
-        else if (error)
-        {
-            std::cerr << "Unknown error while loading font AeroviasBrasil.\r\n";
-        }
-    }
-
-    void setFaceSize()
-    {
-        FT_Set_Char_Size(face, 0, 12*64, 166, 166);
     }
 };
 

d399e4d69496bab3f7bf8817b936083c6f9ef1b4
Jeff Wright <jeffw387@gmail.com>
2018-06-16 12:07:27 -0700

    adding bullet physics

--- main.cpp
+++ main.cpp
@@ -2,15 +2,19 @@
 #undef max
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "TimeHelper.hpp"
-#include "vka/VulkanApp.hpp"
 #include <algorithm>
 #include <iostream>
 #include <map>
 #include <optional>
+#include <variant>
+#include <memory>
 
+#include "vka/VulkanApp.hpp"
+#include "TimeHelper.hpp"
 #include "ECSComponents.hpp"
 #include "entt.hpp"
+#include "btBulletDynamicsCommon.h"
+
 #undef min
 #undef max
 
@@ -32,6 +36,7 @@ class ClientApp : public vka::VulkanApp
 {
 public:
     entt::DefaultRegistry enttRegistry;
+	
 
     void LoadImages()
     {
@@ -95,9 +100,49 @@ public:
     }
 };
 
+struct RigidBodyUnique
+{
+	std::unique_ptr<btRigidBody> rigidBody;
+	std::unique_ptr<btDefaultMotionState> motionState;
+};
+
+RigidBodyUnique CreateRigidBody(
+	btCollisionShape* collisionShape, 
+	btScalar mass,
+	btTransform transform)
+{
+	RigidBodyUnique result;
+
+	auto localInertia = btVector3(0, 0, 0);
+	collisionShape->calculateLocalInertia(mass, localInertia);
+
+	auto startTransform = btTransform();
+	startTransform.setIdentity();
+	result.motionState = std::make_unique<btDefaultMotionState>(startTransform);
+
+	auto rigidBodyInfo = btRigidBody::btRigidBodyConstructionInfo(mass, result.motionState.get(), collisionShape, localInertia);
+
+	result.rigidBody = std::make_unique<btRigidBody>(rigidBodyInfo);
+}
+
+struct SquareCollisionShape
+{
+
+};
+
 int main()
 {
     ClientApp app;
+	auto collisionConfig = std::make_unique<btDefaultCollisionConfiguration>();
+	auto collisionDispatcher = std::make_unique<btCollisionDispatcher>();
+	auto collisionBroadPhase = std::make_unique<btDbvtBroadphase>();
+	auto constraintSolver = std::make_unique<btSequentialImpulseConstraintSolver>();
+	auto dynamicsWorld = std::make_unique<btDiscreteDynamicsWorld>();
+
+	auto collisionShapes = std::vector<std::unique_ptr<btCollisionShape>>();
+	auto rigidBodies = std::vector<btRigidBody>();
+
+	dynamicsWorld->setGravity(btVector3(0, 0, 0));
     
     app.inputBindMap[vka::MakeSignature(GLFW_KEY_LEFT, GLFW_PRESS)]  = 	Bindings::Left;
     app.inputBindMap[vka::MakeSignature(GLFW_KEY_RIGHT, GLFW_PRESS)] = 	Bindings::Right;
@@ -123,6 +168,8 @@ int main()
             cmp::Color(glm::vec4(1.f)),
             cmp::RectSize());
     }
+
+
 
     app.Run(std::string(ConfigPath), std::string(VertexShaderPath), std::string(FragmentShaderPath));
 }
